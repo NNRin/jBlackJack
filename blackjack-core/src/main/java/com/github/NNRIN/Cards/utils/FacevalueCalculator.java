1c60ec97e9b9c8f9040bf1595c7cde072bd1ff89
Nick Rindlisbacher <nicknoah.rindlisbacher@gmail.com>
2025-12-24 23:17:28 +0100

    refactor(Cards): change FacevalueCalculator
    Insteaf of always returning an array of size two, only two values are returned if they differ.

--- blackjack-core/src/main/java/com/github/NNRIN/Cards/utils/FacevalueCalculator.java
+++ blackjack-core/src/main/java/com/github/NNRIN/Cards/utils/FacevalueCalculator.java
@@ -10,15 +10,15 @@ public class FacevalueCalculator implements IFacevalueCalculator {
         return switch (card.facevalue()) {
             case Facevalues.Cut -> throw new IllegalStateException("CUT cards need to be handled explicitly");
             case Facevalues.Ace -> new int[]{1, 11};
-            case Facevalues.Two -> new int[]{2, 2};
-            case Facevalues.Three -> new int[]{3, 3};
-            case Facevalues.Four -> new int[]{4, 4};
-            case Facevalues.Five -> new int[]{5, 5};
-            case Facevalues.Six -> new int[]{6, 6};
-            case Facevalues.Seven -> new int[]{7, 7};
-            case Facevalues.Eight -> new int[]{8, 8};
-            case Facevalues.Nine -> new int[]{9, 9};
-            case Facevalues.Ten, Facevalues.Jack, Facevalues.Queen, Facevalues.King -> new int[]{10, 10};
+            case Facevalues.Two -> new int[]{2};
+            case Facevalues.Three -> new int[]{3};
+            case Facevalues.Four -> new int[]{4};
+            case Facevalues.Five -> new int[]{5};
+            case Facevalues.Six -> new int[]{6};
+            case Facevalues.Seven -> new int[]{7};
+            case Facevalues.Eight -> new int[]{8};
+            case Facevalues.Nine -> new int[]{9};
+            case Facevalues.Ten, Facevalues.Jack, Facevalues.Queen, Facevalues.King -> new int[]{10};
             default -> throw new IllegalArgumentException("Unknown face value: " + card.facevalue());
         };
     }
