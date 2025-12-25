b0595584e50ab496a1c1f78bb81384be06a8f053
Nick Rindlisbacher <nicknoah.rindlisbacher@gmail.com>
2025-12-25 12:01:08 +0100

    feat(Player): add interface and implementation
    The Player class for the Blackjack holds all the data needed for a player Object and implements the necessary methods a game Controller needs to delegate the game flow of a such object.

---
+++ blackjack-core/src/main/java/com/github/NNRIN/Participants/Player.java
@@ -0,0 +1,234 @@
+package com.github.NNRIN.Participants;
+
+import com.github.NNRIN.Cards.Card;
+import com.github.NNRIN.Cards.Facevalues;
+import com.github.NNRIN.Cards.utils.interfaces.IFacevalueCalculator;
+import com.github.NNRIN.Components.Hand;
+import com.github.NNRIN.Components.interfaces.IHand;
+import com.github.NNRIN.Helper.ParticipantStates;
+import com.github.NNRIN.Helper.interfaces.IPayoutCalculator;
+import com.github.NNRIN.Participants.interfaces.IPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Player implements IPlayer {
+    private List<IHand> hands = new ArrayList<>();
+    private String name;
+    private double credit;
+    private boolean isSurrenderAvailable = false;
+    private boolean isInsuranceBought = false;
+    private double insuranceBet = 0;
+    private boolean insuranceWon = false;
+    private IPayoutCalculator payoutCalculator;
+    private IFacevalueCalculator facevalueCalculator;
+
+    public Player(String name, double credit,
+            IFacevalueCalculator facevalueCalculator,
+            IPayoutCalculator payoutCalculator
+    ) {
+        this.name = name;
+        this.credit = credit;
+        this.facevalueCalculator = facevalueCalculator;
+        this.payoutCalculator = payoutCalculator;
+        initHand();
+    }
+
+    private void initHand() {
+        hands.add(new Hand(facevalueCalculator, payoutCalculator));
+        hands.getFirst().setStatus(ParticipantStates.Preparing);
+    }
+
+    @Override
+    public List<IHand> getHand() {
+        return hands;
+    }
+
+    @Override
+    public void setHand(List<IHand> hand) {
+        hands = hand;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getCredit() {
+        return credit;
+    }
+
+    @Override
+    public boolean isGameOver() {
+        return credit == 0 && hands.stream().map(h -> h.getBet()).reduce(0.0, (a, b) -> a + b) == 0;
+    }
+
+    @Override
+    public boolean isSplitAvailable() {
+        // enough credit needs to be there to split and ace's can only be split once
+        try{
+            IHand onTurnHand = getHandOnTurn();
+            return credit >= onTurnHand.getBet() && onTurnHand.isSplittable() &&
+                    ((!onTurnHand.getCards().get(0).facevalue().equals(Facevalues.Ace)) || hands.size() == 1);
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isDoubleDownAvailable() {
+        try{
+            IHand onTurnHand = getHandOnTurn();
+            return onTurnHand.getBet() <= credit && onTurnHand.isDoubleDownAvailable();
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    private IHand getHandOnTurn() throws  Exception{
+        Optional<IHand> onTurn = hands.stream()
+                .filter(h -> h.getStatus() == ParticipantStates.OnTurn).findFirst();
+
+        if(!onTurn.isPresent())
+            throw new IllegalStateException("No Hand is onTurn");
+
+        return onTurn.get();
+    }
+
+    @Override
+    public boolean isSurrenderAvailable() {
+        return isSurrenderAvailable;
+    }
+
+    @Override
+    public void setIsSurrenderAvailable(boolean isSurrenderAvailable) {
+        this.isSurrenderAvailable = isSurrenderAvailable;
+    }
+
+    @Override
+    public boolean isInsuranceAvailable() {
+        if (hands.size() > 1 || hands.get(0).getCards().size() > 2) {
+            return false;
+        }
+        return credit >= hands.get(0).getBet() / 2;
+    }
+
+    @Override
+    public boolean isInsuranceBought() {
+        return isInsuranceBought;
+    }
+
+    @Override
+    public double getInsuranceBet() {
+        return insuranceBet;
+    }
+
+    @Override
+    public boolean isInsuranceWon() {
+        return insuranceWon;
+    }
+
+    @Override
+    public void setBet(double bet) {
+        if(bet > credit || bet < 0)
+            throw new RuntimeException("bet amount is not smaller than credit or above 0");
+
+        hands.get(0).setBet(bet); // guaranteed to be only one hand at betting stage
+    }
+
+    @Override
+    public void split(Card c1, Card c2) {
+        try {
+            if (!isSplitAvailable())
+                throw new Exception("Not splittable");
+
+            IHand onTurnHand = getHandOnTurn();
+
+            boolean aceSplit = onTurnHand.getCards().get(0).facevalue().equals(Facevalues.Ace);
+
+            credit -= onTurnHand.getBet();
+            IHand newHand = onTurnHand.split();
+            hands.add(newHand);
+
+            onTurnHand.addCard(c1);
+            newHand.addCard(c2);
+
+            if (aceSplit) {
+                onTurnHand.setStatus(ParticipantStates.FinishedTurn);
+                newHand.setStatus(ParticipantStates.FinishedTurn);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void doubleDown(Card c) {
+        try {
+            IHand handOnTurn = getHandOnTurn();
+            if(!isDoubleDownAvailable())
+                throw new Exception("DoubleDown not available");
+            handOnTurn.addCard(c);
+            credit -= handOnTurn.getBet();
+            handOnTurn.setBet(handOnTurn.getBet()*2);
+            handOnTurn.setStatus(ParticipantStates.FinishedTurn);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void surrender() {
+        try {
+            IHand handOnTurn = getHandOnTurn();
+            if(!isSurrenderAvailable())
+                throw new Exception("Surrender not available");
+
+            handOnTurn.setStatus(ParticipantStates.Surrendered);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void insure() {
+            if(!isInsuranceAvailable())
+                throw new RuntimeException("Insurance not available");
+
+            insuranceBet = hands.get(0).getBet() / 2;
+            credit -= insuranceBet;
+            isInsuranceBought = true;
+    }
+
+    @Override
+    public void payoutWinnings() {
+        for (IHand h : hands) {
+            credit += h.getRoundPayout();
+        }
+    }
+
+    @Override
+    public void triggerInsuranceWin() {
+        if (!isInsuranceBought())
+            throw new RuntimeException("No Insurance Bought");
+
+        credit += insuranceBet + insuranceBet * 2;
+        insuranceWon = true;
+    }
+
+    @Override
+    public void reset() {
+        insuranceWon = false;
+        insuranceBet = 0;
+        isInsuranceBought = false;
+        isSurrenderAvailable = false;
+
+
+        hands.clear();
+        initHand();
+    }
+}
---
+++ blackjack-core/src/main/java/com/github/NNRIN/Participants/interfaces/IPlayer.java
@@ -0,0 +1,52 @@
+package com.github.NNRIN.Participants.interfaces;
+
+import com.github.NNRIN.Cards.Card;
+import com.github.NNRIN.Components.interfaces.IHand;
+
+import java.util.List;
+
+public interface IPlayer {
+    List<IHand> getHand();
+
+    void setHand(List<IHand> hand);
+
+    String getName();
+
+    double getCredit();
+
+    boolean isGameOver();
+
+    boolean isSplitAvailable();
+
+    boolean isDoubleDownAvailable();
+
+    boolean isSurrenderAvailable();
+
+    boolean isInsuranceAvailable();
+
+    boolean isInsuranceBought();
+
+    double getInsuranceBet();
+
+    boolean isInsuranceWon();
+
+    // set on hand-on-turn's bet
+    void setBet(double bet);
+
+    void setIsSurrenderAvailable(boolean isSurrenderAvailable);
+
+    void split(Card c1, Card c2);
+
+    void doubleDown(Card c);
+
+    void surrender();
+
+    void insure();
+
+    void payoutWinnings();
+
+    void triggerInsuranceWin();
+
+    void reset();
+
+}
