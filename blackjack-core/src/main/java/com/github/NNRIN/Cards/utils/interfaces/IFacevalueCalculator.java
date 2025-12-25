4eba21af747b5b51ec3f8d5e65783d37b55b7535
Nick Rindlisbacher <nicknoah.rindlisbacher@gmail.com>
2025-12-25 01:07:31 +0100

    refactor(FacevalueCalculator): only return single int
    Insteaf of returning an array containing the two possible values for a Card, we only return one value. This means that for an ace we now have defined that it will always have the value 11 returned from the FacevalueCalculator, the calling class then needs to handle this mechanic.

--- blackjack-core/src/main/java/com/github/NNRIN/Cards/utils/interfaces/IFacevalueCalculator.java
+++ blackjack-core/src/main/java/com/github/NNRIN/Cards/utils/interfaces/IFacevalueCalculator.java
@@ -3,5 +3,5 @@ package com.github.NNRIN.Cards.utils.interfaces;
 import com.github.NNRIN.Cards.Card;
 
 public interface IFacevalueCalculator {
-    int[] getCardValue(Card card);
+    int getCardValue(Card card);
 }
