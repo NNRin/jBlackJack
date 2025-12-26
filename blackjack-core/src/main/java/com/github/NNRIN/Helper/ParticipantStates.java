1ee032e962aecb981193c03066f458be296fa660
Nick Rindlisbacher <nicknoah.rindlisbacher@gmail.com>
2025-12-26 21:07:32 +0100

    refactor(Hand States): remove obsolete UnnaturalBlackJack state
    The ParticipantState is no longer needed as it is functionally the same as the Winner State.

--- blackjack-core/src/main/java/com/github/NNRIN/Helper/ParticipantStates.java
+++ blackjack-core/src/main/java/com/github/NNRIN/Helper/ParticipantStates.java
@@ -7,9 +7,8 @@ public enum ParticipantStates {
     WaitingForTurn,
     OnTurn,
     FinishedTurn,
-    Winner, // Pays back Bet + 1x Bet
+    Winner, // Pays back Bet + 1x Bet, also applied for unnatural BlackJack
     BlackJack, // Pays back Bet + 1.5x Bet
-    UnnaturalBlackJack, // Pays Bet + 1x Bet, occurs when the Hand has BlackJack and originated as a Split Hand
     Push, // Tie, pays back the Bet
     Loser, // Pays back nothing
     Surrendered, // Pays back 0.5x Bet
