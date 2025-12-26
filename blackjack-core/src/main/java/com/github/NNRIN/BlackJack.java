5bf692ea8d08f3209100379134f0517c556efd54
Nick Rindlisbacher <nicknoah.rindlisbacher@gmail.com>
2025-12-26 22:04:52 +0100

    feat(BlackJack): implement factory method for SinglePlayerBlackJack

--- blackjack-core/src/main/java/com/github/NNRIN/BlackJack.java
+++ blackjack-core/src/main/java/com/github/NNRIN/BlackJack.java
@@ -1,4 +1,26 @@
 package com.github.NNRIN;
 
+import com.github.NNRIN.Cards.utils.FacevalueCalculator;
+import com.github.NNRIN.Cards.utils.interfaces.IFacevalueCalculator;
+import com.github.NNRIN.Components.PlayingDeck;
+import com.github.NNRIN.Components.RoundResultCalculator;
+import com.github.NNRIN.Components.SinglePlayerGameManager;
+import com.github.NNRIN.Components.interfaces.ISingePlayerGameManager;
+import com.github.NNRIN.Helper.PayoutCalculator;
+import com.github.NNRIN.Helper.interfaces.IPayoutCalculator;
+import com.github.NNRIN.Participants.Dealer;
+import com.github.NNRIN.Participants.Player;
+
 public class BlackJack {
+    public static ISingePlayerGameManager getSingleplayerGame() {
+        IFacevalueCalculator facevalueCalculator = new FacevalueCalculator();
+        IPayoutCalculator payoutCalculator = new PayoutCalculator();
+
+        return new SinglePlayerGameManager(new Dealer(facevalueCalculator, payoutCalculator),
+                new Player("You", 10_000, facevalueCalculator, payoutCalculator),
+                new PlayingDeck(6),
+                new RoundResultCalculator()
+        );
+
+    }
 }
